f8d00ad490eaeb8f593efc26e17073c3abf853d3
minjongkim <rlaalswhd200@naver.com>
2021-10-26 13:45:48 +0900

    now

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    //start now
+    //start now changed
 
 
 }
