5ae402824792e5b7e9b2bf168e4cda262454b102
minjongkim <rlaalswhd200@naver.com>
2021-10-26 13:48:29 +0900

    2

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    //start now changed
+    //start now changed how to upload to github? changed?
+    
 
 
 }
